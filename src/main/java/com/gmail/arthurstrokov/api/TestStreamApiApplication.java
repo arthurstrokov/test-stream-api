ee0c5b78c75fca1a58b2191641f2ad1165163df1
Arthur Strokov <arthurstrokov@gmail.com>
2022-06-15 20:54:18 +0300

    start

---
+++ src/main/java/com/gmail/arthurstrokov/api/TestStreamApiApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.arthurstrokov.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStreamApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestStreamApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmail/arthurstrokov/api/TestStreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.arthurstrokov.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestStreamApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
